21920f7204a194cf0b5310314d3e20903303153f
João Pedro Lopes <joaopalopes24@gmail.com>
2026-02-21 12:05:52 -0300

    feat(tailwind): adding tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-8">
+    <h1 class="text-3xl font-bold text-gray-800">You did it!</h1>
+
+    <p class="mt-4 text-gray-600">
+      Visit
+      <a
+        rel="noopener"
+        target="_blank"
+        href="https://vuejs.org/"
+        class="text-blue-600 hover:underline"
+      >
+        vuejs.org
+      </a>
+      to read the documentation
+    </p>
+  </div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
+import './style.css'
+import App from './App.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
 
 const app = createApp(App)
 
